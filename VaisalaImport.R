82212d8af43f12d5c311175b8174e395f9609f92
Péter Kalicz <kaliczp@gmail.com>
2020-11-15 20:04:56 +0100

    Whole file is read as raw, precessing header is refactored
    Whole file is read as raw, for better processing. Header now extracted in finer way. Same Head.df is processed, without binary characters.

--- VaisalaImport.R
+++ VaisalaImport.R
@@ -2,16 +2,43 @@ VaisalaImport <- function(filename, column = 25){
     ## Determination of size in bytes
     size <- file.info(filename)$size
     stopifnot(size > 0) ## check case of empty file
-    ### Process header
+### Read whole file
     newdata <- file(filename, "rb")
-    rawHead <- readBin(newdata, "character", n=400)
-    HeadNames <- rawHead[seq(7, by = 6, length=66)]
-    HeadTypes <- rawHead[seq(10, by = 6, length=66)]
-    Head.df <- data.frame(HeadNames, HeadTypes)
-    rawData <- readBin(newdata, "raw", n = size - 400)
+    rawData <- readBin(newdata, "raw", n = size)
     close(newdata)
-    ## Determine beginning of the values
     tempStart <- which(rawData == "00")
+### Process header
+    ## End of header
+    tempHeadEnd <- which(rawData == as.raw("0xfe"))
+    tempHeadEndOK <- rawData[tempHeadEnd + 1] == as.raw("0xff")
+    HeadEnd <- tempHeadEnd[tempHeadEndOK]
+    if(length(HeadEnd) > 1){
+        warning("HeadEnd longer than one! First is used.")
+        HeadEnd <- HeadEnd[1]
+    }
+    ## Header strings
+    tempStartOK <- rawData[tempStart + 1] == "00" & rawData[tempStart + 2] == "00"
+    StartHead <- tempStart[tempStartOK]
+    if(rawToChar(rawData[(StartHead[1]+3):(StartHead[2]-1)]) != "B")
+        warning("First character is not B!")
+    ## Substract var names locations
+    StartVarName <- StartHead[-1]
+    StartVarName <- c(StartVarName[StartVarName < HeadEnd], HeadEnd)
+    ## Create pointer to substarction
+    HeadPointer <- seq(1, length(StartVarName) - 1, by = 2)
+    HeadTextLength <- length(HeadPointer)
+    ## Create empty variables
+    HeadNames <- character(HeadTextLength)
+    HeadTypes <- character(HeadTextLength)
+    ## Varname substraction loop
+    for(tti in 1:HeadTextLength) {
+        HeadIndex <- HeadPointer[tti]
+        HeadNames[tti] <- rawToChar(rawData[(StartVarName[HeadIndex] + 4):(StartVarName[HeadIndex + 1] - 1)])
+        HeadTypes[tti] <- rawToChar(rawData[(StartVarName[HeadIndex + 1] + 4):(StartVarName[HeadIndex + 2] - 2)])
+    }
+    Head.df <- data.frame(HeadNames, HeadTypes)
+### Process data
+    ## Determine beginning of the values
     tempStartOK <- rawData[tempStart + 1] == "01"
     Start <- tempStart[tempStartOK]
     ## if(!all(paste0(as.character(rawData[Start]),as.character(rawData[Start+1])) == "0001"))
